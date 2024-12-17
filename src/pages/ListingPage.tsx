2d6d69e0ffbf7cccdc2ec4a3693e8f849cc0e42c
rpraneeth5225 <praneethregonda28@gmail.com>
2024-12-17 00:01:07 -0500

    Styling changes made in listing page

--- src/pages/ListingPage.tsx
+++ src/pages/ListingPage.tsx
@@ -15,7 +15,7 @@ export default function ListingPage() {
   }
 
   return (
-    <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
+    <div className="max-w-full mx-auto px-4 sm:px-6 lg:px-8 py-8">
       <ListingDetails listing={listing} />
     </div>
   );
